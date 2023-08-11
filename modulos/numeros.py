f5a3caffea0442d66c27b523860c1c99f9ad24fe
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-08-11 19:33:30 -0300

    new

---
+++ modulos/numeros.py
@@ -0,0 +1,6 @@
+from uteis import numeros
+
+num = int(input('Digite um valor para Fatorizar: '))
+print(f'O Fatorial de {num} é {numeros.fatorial(num)}.')
+print(f'O Dobro de {num} é {numeros.dobro(num)}')
+print(f'O Triplo de {num} é {numeros.triplo(num)}')
