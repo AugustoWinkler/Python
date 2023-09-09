9b98d84f39ab2e60df7f89c16cd3749170228e37
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-09-09 19:38:26 -0300

    Update main.py

--- LaserCalculator/main.py
+++ LaserCalculator/main.py
@@ -1,6 +1,32 @@
 #Calculadora de Custo Hora CNC Laser +  Materials
 
 from modulos import *
+from tkinter import *
+
+app=Tk()
+app.title("Calculadora Laser")
+app.geometry("720x486")
+app.configure(background="#dde")
+
+
+#Sessão da Máquina
+maquina = LabelFrame(app, text="Máquina", borderwidth=1,relief="solid",width=350,height=150).grid(row=1,column=1,columnspan=2,rowspan=3)
+#Preço da Máquina.
+maquinavalor = Label(maquina, text="Valor Máquina:").grid(row=1,column=1)
+maquinavaloren = Entry(maquina).grid(row=1,column=2)
+#Valor Residual da Máquina.
+residualvalor = Label(maquina, text="Valor Residual::").grid(row=2,column=1)
+residualvaloren = Entry(maquina).grid(row=2,column=2)
+#Vida Util da Máquina.
+vidautilmaquina = Label(maquina, text="Valor Máquina:").grid(row=3,column=1)
+vidautilmaquinaen = Entry(maquina).grid(row=3,column=2)
+
+
+#Sessão do Laser
+laser = LabelFrame(app,text="Laser", borderwidth=1, relief="solid", width=350, height=150).place(y=200)
+valorlaser = Label(laser, text="Laser").grid(row=1,column=1)
+valorlaseren = Entry().grid(row=1,column=2)
+
 
 
 valor_Maquina = float(3000)
@@ -13,7 +39,6 @@ horas_DiasTrabalhados = int(8)
 despesas_Operacionais = float(200) #Consumo de Energia Elétrica,Custos Manutenção e Reparos,Aluguel, Outros..
 operador = float(2000)
 
+app.mainloop()
 
 
-print(f"{teste(despesas_Operacionais,dias_Trabalhados):.2f}R$")
-print(hora_Laser(vida_UtilLaser,vida_UtilLaser))
