b0a068a7b31c8a49382eac59d4877217bd9a1b18
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-10-26 23:23:23 -0300

    Update functions.py

--- LaserCalculator/Novo Projeto/functions.py
+++ LaserCalculator/Novo Projeto/functions.py
@@ -23,3 +23,7 @@ def hora_Laser(custo_Laser,vida_UtilLaser):
 def Depreciacao_Anual():
     depreciacao = (valor_Maquina - valor_Residual)/vida_UtilMaquina
     return depreciacao
+
+def Calcular_Hora_Maquina():
+    hora = 0
+    return hora
