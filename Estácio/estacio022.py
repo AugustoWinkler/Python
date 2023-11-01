edc16d6fd4479fc15e35e456ba727977415729ca
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-11-01 16:56:26 -0300

    n

---
+++ Estácio/estacio022.py
@@ -0,0 +1,12 @@
+nomes = ["João", "Maria", "Ana"] #Lista/Array
+
+print(nomes) #Exibe a Lista/Arrays
+
+nomes.append("Gustavo") #Adiciona a String Gustavo ao final da Lista/Array no caso Indice 3
+print(nomes)
+
+nomes.remove("João") #Remove o Elemento João pelo seu valor
+print(nomes)
+
+nomes.pop(2) #Remove o Elemento de Indice 2
+print(nomes)
