921534bc7b65a2bbdad7421bd1e85086a6a5d7c9
AugustoWinkler <111334249+AugustoWinkler@users.noreply.github.com>
2023-11-06 17:03:53 -0300

    Create estacio026.py

---
+++ Estácio/estacio026.py
@@ -0,0 +1,16 @@
+def trocar(seq,i):
+    aux=seq[i]
+    seq[i]=seq[i+1]
+    seq[+1]=aux
+seq = [1,15,22,6,7,19,8,3,5,20]
+troca=1
+while troca:
+    troca = 0
+    i = 0
+    for i in range(len(seq)-1):
+        if seq[i]>seq[i+1]:
+            trocar(seq,i)
+            troca=1
+
+
+print(seq)
